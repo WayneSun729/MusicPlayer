c955c7e8fc4eb93c08e5abc6aab8adaa378f362f
Wayne <1127113613@qq.com>
2021-07-30 15:01:15 +0800

    完成了底部控制控件的设计

---
+++ app/src/main/java/com/wayne/musicplayer/ControlCenter.java
@@ -0,0 +1,52 @@
+package com.wayne.musicplayer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+/**
+ * @author Wayne
+ */
+public class ControlCenter extends LinearLayout implements View.OnClickListener {
+
+
+    public ControlCenter(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.buttons_control_center, this);
+
+
+        Button btnPreSong = findViewById(R.id.btn_preSong);
+        btnPreSong.setOnClickListener(this);
+        Button btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(this);
+        Button btnPause = findViewById(R.id.btn_pause);
+        btnPause.setOnClickListener(this);
+        Button btnNextSong = findViewById(R.id.btn_nextSong);
+        btnNextSong.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_preSong:
+                Toast.makeText(MyApplication.context, "上一首",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_start:
+                Toast.makeText(MyApplication.context, "开始",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_pause:
+                Toast.makeText(MyApplication.context, "暂停",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_nextSong:
+                Toast.makeText(MyApplication.context, "下一首",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+}
