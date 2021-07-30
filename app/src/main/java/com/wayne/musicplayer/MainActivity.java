f872072c599554c7ddcd5edcc49d73aa78843ad3
Wayne <1127113613@qq.com>
2021-07-30 17:36:13 +0800

    实现了播放/暂停播放res下的一首固定歌曲

--- app/src/main/java/com/wayne/musicplayer/MainActivity.java
+++ app/src/main/java/com/wayne/musicplayer/MainActivity.java
@@ -8,10 +8,19 @@ import android.os.Bundle;
  * @author Wayne
  */
 public class MainActivity extends AppCompatActivity {
+    ControlCenter controlCenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controlCenter = findViewById(R.id.controlCenter);
+        controlCenter.setMusic(R.raw.example);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        controlCenter.myMediaPlayer.stop();
     }
 }
